1ddc26b88656e69427e2d3f9d6c6d8f1ef7b8e8e
Pablo Perera Moreno <pablomoreeno12@gmail.com>
2022-09-22 16:07:03 +0100

    Added json data

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 const port = 4000;
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
 
-app.use(cors())
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg =
+                "The CORS policy for this site does not " +
+                "allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}))
 
-app.get('/api/digimon', async (request, response) => {
-    const data: Array<any> = [];
+app.get('/api/digimon', (request, response) => {
+    const raw = fs.readFileSync('./server/resources/digimon.json', 'utf-8');
+    const data = JSON.parse(raw);
     response.send(data)
+    response.end();
 });
 
 app.listen(port, () => {
